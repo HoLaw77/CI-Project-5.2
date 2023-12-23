551b2b6c43b3994b17fbe8bf7c57f9ebe4abe07b
HoLaw77 <fh21hwl@leeds.ac.uk>
2023-12-23 14:13:59 +0000

    Set display ordering in productadmin

--- product/admin.py
+++ product/admin.py
@@ -2,8 +2,21 @@ from django.contrib import admin
 from .models import Language, Category, Product, ProductImage
 # Register your models here.
 
-admin.site.register(Language)
-admin.site.register(Category)
+
+
+class ProductAdmin (admin.ModelAdmin):
+    list_display = (
+    'name',
+    'publisher', 
+    'author', 
+    'year_of_publication',
+    'isbn',
+    'price',
+    )
+
+    ording = ('name',) 
+
 admin.site.register(Product)
 admin.site.register(ProductImage)
-
+admin.site.register(Language)
+admin.site.register(Category)
