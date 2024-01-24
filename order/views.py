b60d1a719f603d51b686529643647d6bd596291a
HoLaw77 <fh21hwl@leeds.ac.uk>
2024-01-24 13:43:05 +0000

    debug for remove order

--- order/views.py
+++ order/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render, redirect, reverse
-from django.http import Http404
+from django.http import Http404, JsonResponse
 from django.shortcuts import HttpResponse, get_object_or_404
 from product.models import Product, ProductImage
 from django.contrib import messages
@@ -24,7 +24,7 @@ def add_order(request, books_id):
     else:
         bag[int(books_id)] = quantity
         messages.success(request, f'Added {product.name} quantity to your bag')
-    item_items = bag.get('item_items')
+    # item_items = bag.get('item_items')
     request.session['bag'] = bag
     
 
@@ -55,28 +55,28 @@ def adjust_order(request, item_id):
             return render (request, 'order/order.html')
     else: 
         bag.pop(item_id)
+
     request.session['bag'] = bag
     return redirect(reverse('order'))
 
 
 def remove_order(request, item_id):
     """Remove individual product from the cart"""
+    item_id = str(item_id)
     try:
         print("Entering remove_order view")
         print("Removing item with ID:", item_id)
         product = get_object_or_404(Product, id=item_id)
         bag = request.session.get('bag', {})
 
-        if str(item_id) in bag:
-            product_id = str(item_id)
-            product = bag[product_id]
-            del bag[product_id]
-            request.session['bag'] = bag
-        
-        messages.success(request, f'Removed {product} from your cart')
+        if item_id in list(bag.keys()):
+            bag.pop(item_id)
+            
+            messages.success(request, f'Removed {product} from your cart')
+            print(f"Removed {product}")
+            return JsonResponse({'message': f'Removed {product} from your cart'})
+            # return HttpResponse(status=200)
         request.session['bag'] = bag
-        print("Exiting remove_order view successfully")
-        return HttpResponse(status=200)
     except Http404 as e:
         messages.error(request, f'Error removing item: {e}')
         print(f"Http404 Error: Product not found")
@@ -85,7 +85,8 @@ def remove_order(request, item_id):
         messages.error(request, f'Error removing item: {e}')
         print(f"Error: {e}")
         return HttpResponse(status=500)
-
+    
+    return redirect (reverse('order'))
 
 def remove_all(request):
     
