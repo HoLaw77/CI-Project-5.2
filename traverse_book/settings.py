c30b20dd319caacce9c6bc25e3425d6912773cd4
HoLaw77 <fh21hwl@leeds.ac.uk>
2024-01-27 11:31:11 +0000

    update settings on debug

--- traverse_book/settings.py
+++ traverse_book/settings.py
@@ -29,7 +29,7 @@ CSRF_TRUSTED_ORIGINS = ['https://8000-holaw77-ciproject52-946yf26nqxg.ws-eu107.g
 SECRET_KEY = os.environ.get('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = "DEVELOPMENT" in os.environ
+DEBUG = "DEBUG" in os.environ
 
 ALLOWED_HOSTS = ['8000-holaw77-ciproject52-946yf26nqxg.ws-eu107.gitpod.io', 'ci-project5-4ad812effe24.herokuapp.com']
 
