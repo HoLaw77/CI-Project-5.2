916422008c7db8d326980be8ee503e41115a574e
HoLaw77 <fh21hwl@leeds.ac.uk>
2024-01-28 17:16:04 +0000

    delete repeated code in forms.py

--- checkout/forms.py
+++ checkout/forms.py
@@ -21,27 +21,14 @@ def __init__(self, *args, **kwargs):
         "postcode": "Postcode", 
         "country": "Country",
     }
-    from django import forms
-from .models import Order
+    
+    self.fields['full_name'].required = True
+    self.fields['email'].required = True
+    self.fields['address1'].required = True
+    self.fields['country'].required = True
 
-class ConfirmOrder(forms.ModelForm):
-    class Meta:
-        model = Order
-        fields = ("full_name", "country", 
-    "email", "phone_number", "address1", "address2", "postcode")
+    for field in self.fields:
+        if self.fields[field].required:
+            placeholder = f'{placeholder[field]} *'
 
-def __init__(self, *args, **kwargs):
-    """Add placeholder for form fields"""
-
-    super().__init__(*args, **kwargs)
-    placeholders = {
-
-        "full_name": "Full Name",  
-        "email": "Your Email", 
-        "phone_number": "Phone Number", 
-        "address1": "Address Line 1", 
-        "address2": "Address Line 2", 
-        "postcode": "Postcode", 
-        "country": "Country",
-    }
     
