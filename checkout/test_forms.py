87018041cecaf8406973900255876312f22e7bcc
HoLaw77 <fh21hwl@leeds.ac.uk>
2024-01-30 13:09:36 +0000

    write test for checkout form

---
+++ checkout/test_forms.py
@@ -0,0 +1,47 @@
+from django.test import TestCase
+from .models import Order, OrderDetail
+from .forms import ConfirmOrder
+
+class ConfirmOrderForm(TestCase):
+
+    def test_full_name_is_required(self):
+        """test full_name is required""" 
+        form = ConfirmOrder({'full_name': ''})
+        self.assertFalse(form.is_valid())
+        self.assertIn('full_name', form.errors.keys())
+        self.assertEqual(form.errors['full_name'][0], 'This field is required.')
+    
+    def test_country_is_required(self):
+        """test country is required""" 
+        form = ConfirmOrder({'country': ''})
+        self.assertFalse(form.is_valid())
+        self.assertIn('country', form.errors.keys())
+        self.assertEqual(form.errors['country'][0], 'This field is required.')
+    
+    def test_email_is_required(self):
+        """test email is required"""
+        form = ConfirmOrder({'email': ''})
+        self.assertFalse(form.is_valid())
+        self.assertIn('email', form.errors.keys())
+        self.assertEqual(form.errors['email'][0], 'This field is required.')
+
+    def test_address1_is_required(self):
+        """test address1 is required"""
+        form = ConfirmOrder({'address1': ''})
+        self.assertFalse(form.is_valid())
+        self.assertIn('address1', form.errors.keys())
+        self.assertEqual(form.errors['address1'][0], 'This field is required.')
+
+    def test_address2_is_required(self):
+        """test address2 is required"""
+        form = ConfirmOrder({'address2': ''})
+        self.assertFalse(form.is_valid())
+        self.assertIn('address2', form.errors.keys())
+        self.assertEqual(form.errors['address2'][0], 'This field is required.')
+
+    def test_postcode_is_required(self):
+        """test postcode is required"""
+        form = ConfirmOrder({'postcode': ''})
+        self.assertFalse(form.is_valid())
+        self.assertIn('postcode', form.errors.keys())
+        self.assertEqual(form.errors['postcode'][0], 'This field is required.')
