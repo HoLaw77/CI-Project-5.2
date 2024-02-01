1837832b3eb241cea67984ecad0aced2baeb2b49
HoLaw77 <fh21hwl@leeds.ac.uk>
2024-02-01 21:12:19 +0000

    writing test for product models

--- product/test_models.py
+++ product/test_models.py
@@ -47,4 +47,25 @@ class TestModels(TestCase):
         region = Category.objects.get(region=4)
         self.assertEqual(region.get_region_display(), 'France')
 
-    
+    def test_product_cover_choice(self):
+        """Test cover choice of Product being saved in model"""
+        cover = Product.objects.get(cover=1)
+        self.assertEqual(cover.get_cover_display(), 'Hardback')
+
+    def test_product_model_save(self):
+        """Test product info are saved"""
+        book = Product.objects.create(
+            cover = 1,
+            year_of_publication = 1998,
+            name = "La vie",
+            publisher = "folio",
+            author = "Romeo",
+            number_of_pages = 4,
+            isbn = "9780099302780",)
+        book.save()
+        saved_book = Product.objects.get(id=book.id) 
+        self.assertEqual(saved_book.name, "La vie")
+        self.assertEqual(saved_book.publisher, "folio")
+        self.assertEqual(saved_book.author, "Romeo")
+        self.assertEqual(saved_book.number_of_pages, 4)
+        self.assertEqual(saved_book.isbn, "9780099302780")
